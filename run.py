25fa87582e2c5017a63ed57adbc083f39c77ef18
liuxiaoyun <15600560657@163.com>
2020-12-04 17:08:24 +0800

    no message

---
+++ run.py
@@ -0,0 +1,8 @@
+import pytest
+
+
+if __name__ == '__main__':
+    pytest.main()
+# pytest.main(["-sq", "--alluredir", "./allure-results"])
+# os.system(r"allure generate --clean allure-results -o allure-report")
+
